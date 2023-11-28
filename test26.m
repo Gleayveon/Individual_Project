7577034ad5f42919a177ed3de0e8dcfcca8a81a0
gleay <gleay@10.216.76.60>
2023-11-28 01:44:43 +0000

    Add new variable to store all data returned from evaluation.m

--- test26.m
+++ test26.m
@@ -16,6 +16,9 @@ DipCount = 0;
 InterruptionCount = 0;
 Udc_mean = 0;
 Urms_mean = 0;
+Factor_rms = 0;
+Factor_peak_valley = 0;
+RDF_total = 0;
 for num = 1:len
     cd 'A:\Lin project\Individual_Project'
     [Udc_mean_sample,Urms_mean_sample,RDF_eachwindow,Swell_timesum,Dip_timesum,...
@@ -37,4 +40,16 @@ for num = 1:len
     legend('Normal','Swell','Dip','Interruption');
     title('Chart of the data''s voltage status');
     cd 'A:\Lin project\Data_Check'
+    figure(3)
+    subplot(2,1,1)
+    plot(Udc_mean(2:end));
+    title('Udc_mean');
+    hold on
+    subplot(2,1,2)
+    plot(Urms_mean(2:end));
+    title('Urms_mean');
+    Factor_rms = cat(1,Factor_rms, Factor_rms_sample);
+    Factor_peak_valley = cat(1,Factor_peak_valley,Factor_peak_valley_sample);
+    RDF_total = cat(1,RDF_total,RDF_eachwindow);
+
 end
